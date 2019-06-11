b5fe0ddd7f61f3e04f7bffb056cec837f275bccc
Sina Pakseresht <m.sina.pakseresht@gmail.com>
2019-06-11 13:13:32 +0430

    draw circle on master

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode(size=(100, 100))
+screen = pygame.display.set_mode(size=(500, 500))
+
+pygame.draw.circle(screen, (255, 255, 255), (200, 200), 100, 1)
 
 done = False
 
 while not done:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             done = True
             break
