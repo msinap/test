6a0e3a90d7a5eb93faca0a3ae136ba04e00d9439
Sina Pakseresht <m.sina.pakseresht@gmail.com>
2019-06-11 13:03:14 +0430

    drawrect1

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode(size=(100, 100))
+screen = pygame.display.set_mode(size=(200, 200))
 
-done = False
+pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
 
-while not done:
+shouldQuit = False
+
+while not shouldQuit:
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
-            done = True
+            shouldQuit = True
             break
     pygame.display.flip()
+
+# Comment is on MASTERRRRR
